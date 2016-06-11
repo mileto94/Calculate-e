e824e30b20b93e1967573efd9bc1c554a3100933
Milka Ferezliyska <mileto94@abv.bg>
2016-06-11 20:01:34 +0300

    Add multuprocessing

--- code/main.py
+++ code/main.py
@@ -3,16 +3,31 @@ import decimal as dc
 import time
 import argparse
 import multiprocessing
+import numpy as np
+from ctypes import c_longdouble
 
+import dummy
+# dummy.final_sum = dc.Decimal(dummy.final_sum)
 
-SUM = 0
 # p = 2000 ~ Execution time: 22.088331699371338
 
 
 def calculate_current(i):  # noqa
-    # print(i)
+    print(i)
     current = dc.Decimal(pow(3 * i, 2) + 1) / dc.Decimal(factorial(3 * i))
-    return current
+    print('current', current)
+    dummy.final_sum.value += current
+    print('calculate_current', 'id:', i, 'value:', dummy.final_sum.value)
+
+
+# def job_fn(i):  # noqa
+#     dummy.final_sum.value += factorial(i)
+#     print('job_fn', 'id:', i, 'value:', dummy.final_sum.value)
+
+
+def init_process(share):  # noqa
+    dummy.final_sum = share
+    print ("initProcess")
 
 
 def main():  # noqa
@@ -53,14 +68,30 @@ def main():  # noqa
     digits_precision = args.t if args.t else 20000
     dc.getcontext().prec = digits_precision
 
-    global SUM
+    # for i in range(iterations_count):
+    #     SUM += calculate_current(i)
+
+    # allocate shared array - want lock=False in this case since we
+    # aren't writing to it and want to allow multiple processes to access
+    # at the same time - I think with lock=True there would be little or
+    # no speedup
+    result = multiprocessing.Value('f', 0.)
+
+    # fork
+    pool = multiprocessing.Pool(processors_number, initializer=init_process, initargs=(result,))
+
     start = time.time()
 
-    for i in range(iterations_count):
-        SUM += calculate_current(i)
+    jobs = [pool.apply_async(calculate_current, [i]) for i in range(iterations_count)]
 
-    print(SUM)
+    for x in jobs:
+        x.get()
+
+    print(result.value)
     print('Execution time: {0}'.format(time.time() - start))
+
+    pool.close()
+    pool.join()
 
 
 if __name__ == '__main__':
